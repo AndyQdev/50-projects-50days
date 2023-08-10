b2a3a7cb321c2c045b5c7390694ac532ea4977b7
Andres Quispe <andres.03.12.2003@gmail.com>
2023-08-09 23:13:34 -0400

    Count-Infinit

--- contador/main.js
+++ contador/main.js
@@ -2,12 +2,16 @@ const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
 const finalMessage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
+const stopBtn = document.querySelector('#stop')
+let timer;
 
 RunAnimation();
-function RunAnimation(){
-    nums.forEach((num, idx)=>{
-        const NumToLast = nums.length-1;
 
+function RunAnimation(){
+
+    const NumToLast = nums.length-1;
+    /* console.time(); */
+    nums.forEach((num, idx)=>{   
         num.addEventListener('animationend',(e)=>{
             if (e.animationName==="goIn" && NumToLast!==idx){
                 num.classList.remove("in");
@@ -15,12 +19,15 @@ function RunAnimation(){
             } else if (e.animationName ==="goOut" && NumToLast!==idx){
                 num.nextElementSibling.classList.add("in");
             }else{
-                counter.classList.add("hide");
+                /* counter.classList.add("hide");
                 finalMessage.classList.add("show");
-            }
+                console.timeEnd(); */ 
+                /* Console.timeEnd, me ayudan a ver cuanto tarda en ejecutarse la funcion*/
+            };
         })
     });
-}
+}                                        
+
 function ResetDoom(){
     counter.classList.remove("hide");
     finalMessage.classList.remove("show");
@@ -29,6 +36,15 @@ function ResetDoom(){
 }
 
 replay.addEventListener("click", ()=>{
-    ResetDoom();
-    RunAnimation();
+        ResetDoom();
+        RunAnimation();
+        timer = setInterval(()=>{
+        ResetDoom();
+        RunAnimation();
+    }, 3525.629150390625)
+})
+stopBtn.addEventListener("click", ()=>{
+    clearInterval(timer);
+    counter.classList.add("hide");
+    finalMessage.classList.add("show")
 })
