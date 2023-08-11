e98e0ebea3f8a8606ab2122c4fd07be7acb2d9cf
Andres Quispe <andres.03.12.2003@gmail.com>
2023-08-10 22:45:01 -0400

    Project-Navigation

---
+++ navigation/script.js
@@ -0,0 +1,4 @@
+const togle = document.getElementById("togle");
+const navContainer = document.getElementById("nav");
+
+togle.addEventListener("click", ()=>navContainer.classList.toggle("active"));
